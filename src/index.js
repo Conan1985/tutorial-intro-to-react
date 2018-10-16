d95ca3f176547079d8b6f02ba2e1113e7ba5f7e2
Conan1985 <Conan1985@users.noreply.github.com>
2018-10-16 13:44:07 -0400

    Insuert key in li

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ class Game extends React.Component {
                'Go to step #' + move:
                'Go to game start';
            return (
-               <li>
+               <li key={move}>
                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
                </li>
            )
